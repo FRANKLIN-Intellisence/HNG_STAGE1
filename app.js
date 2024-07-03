63b6fa954bab0c67627c82d9646bc4ded5d8d72f
FRANKLIN-Intellisence <franklinsolaty@gmail.com>
2024-07-03 13:13:08 +0100

    My first commit to HNGstage1 repository

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('Time').innerHTML = Date();
+
+const days = new Array();
+days[0] = "Monday";
+days[1] = "Tuesday";
+days[2] = "Wednesday";
+days[3] = "Thursday";
+days[4] = "Friday";
+days[5] = "Saturday";
+days[6] =  "Sunday";
+
+document.getElementById("Day").innerHTML = days[2];
